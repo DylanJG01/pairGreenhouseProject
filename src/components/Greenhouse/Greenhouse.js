7ee3091253eed8472f7bed790859528664938ba6
Vusal Layijov <Layijovvusal@gmail.com>
2023-02-02 13:15:28 -0800

    ThemeContext

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,24 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
-
+import {useTheme} from '../../context/ThemeContext'
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
-
+const {themeName} =useTheme()
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+      {themeName === 'day' && <img className='greenhouse-img'
+        src={dayImage}
+        alt='greenhouse'
       />
+      }
+      {themeName === 'night' && <img className='greenhouse-img'
+        src={nightImage}
+        alt='greenhouse'
+      />
+      }
       <LightSwitch />
       <ClimateStats />
     </section>
